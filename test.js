e5fa241b08d69e26e4448c3db1a105b581552aa4
leomaratsis <maratsis.leo@gmail.com>
2021-01-29 19:12:06 +0000

    Create test.js

---
+++ test.js
@@ -0,0 +1,22 @@
+var price = 200;
+
+function setPrice() {
+    price = 20;
+}
+
+setPrice();
+
+console.log(price);
+console.log(vat);
+
+
+
+// var students = 10;
+
+// function tutorial() {
+//     var tutor = "Aris";
+// }
+
+// tutorial();
+// console.log(students);
+// console.log(tutor);
