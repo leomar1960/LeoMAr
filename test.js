8c3f4d0bb1cb9e9e885bb9a8b165b9d6d5be8adb
leomaratsis <maratsis.leo@gmail.com>
2021-01-29 19:29:27 +0000

    Update test.js
    afdafad

--- test.js
+++ test.js
@@ -20,3 +20,25 @@ console.log(vat);
 // tutorial();
 // console.log(students);
 // console.log(tutor);
+var price = 200;
+
+function setPrice() {
+    price = 20;
+}
+
+setPrice();
+
+console.log(price);
+console.log(vat);
+
+
+
+// var students = 10;
+
+// function tutorial() {
+//     var tutor = "Aris";
+// }
+
+// tutorial();
+// console.log(students);
+// console.log(tutor);
