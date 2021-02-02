73a90c80035207e85a12c7efb5a9d2cbb2d8fd66
leomaratsis <maratsis.leo@gmail.com>
2021-02-02 19:49:45 +0000

    update

--- Chapter_19_Modern_WorkFlow/src/index.js
+++ Chapter_19_Modern_WorkFlow/src/index.js
@@ -3,3 +3,4 @@ import users, { getPremUsers } from './data';
 
 const premUsers = getPremUsers(users);
 console.log(users, premUsers);
+// "babel": "node_modules/.bin/babel ./src/index.js -w -o ./dist/assets/bundle.js",
